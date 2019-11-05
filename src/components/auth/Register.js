4d2b4b4426e95afab9ca8c741366c88ddd876986
Adam Knowles <acknowles524@gmail.com>
2019-11-05 11:42:27 -0600

    fixed login issue

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -30,7 +30,7 @@ const Register = props => {
 
         register(newUser).then(() => {
             props.history.push({
-                pathname: "/login"
+                pathname: "/"
             })
         })
     }
